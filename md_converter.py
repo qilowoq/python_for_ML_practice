2309168fb933e2e8bfa4bcfc2f140a740c51e172
Oleg Dmitriev <85520825+qilowoq@users.noreply.github.com>
2022-10-12 09:44:41 +0300

    Create md_converter.py

---
+++ md_converter.py
@@ -0,0 +1,28 @@
+#title MD Converter
+# description Конвертирует solution.py -> out.txt
+#---end---
+
+
+def get_file(name):
+    with open(name, 'r') as f:
+        return f.read()
+
+def write_file(name):
+    merged = '+ [{}](#{})\n\n## {}\n\n'.format(title, lower_cased_title, title) + \
+    '\n'.join(desc) + '\n\n```python\n{}\n```'.format(code.lstrip('\n'))
+    with open(name, 'w') as f:
+        f.write(merged)
+
+
+info, code = get_file('solution.py').split('\n#---end---')
+desc = []
+for line in info.split('\n'):
+    if line.startswith('# title'):
+        title = line[8:]
+        lower_cased_title = '-'.join(title.lower().split())
+    elif line.startswith('# description'):
+        desc.append(line[14:])
+    else:
+        desc.append(line[1:])
+
+write_file('out.txt')
