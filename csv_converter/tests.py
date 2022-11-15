29987d003b32f782946064533f7f8bf952222d18
Oleg Dmitriev <85520825+qilowoq@users.noreply.github.com>
2022-11-16 02:44:09 +0300

    added hw from 9.11 (lol) frfr

---
+++ csv_converter/tests.py
@@ -0,0 +1,46 @@
+import unittest
+from csv_converter import *
+
+class TestConverter(unittest.TestCase):
+
+    def test_convert_csv_to_json_only_header(self) -> None:
+        c1 = CSVConverter()
+        c2 = CSVConverter2()
+        data = ['id,name,birth,salary,department']
+        self.assertEqual(c1.convert_csv_to_json(data), '[]')
+        self.assertEqual(c2.convert_csv_to_json(data), '[]')
+
+    def test_convert_csv_to_json(self) -> None:
+        c1 = CSVConverter()
+        c2 = CSVConverter2()
+        data = ['id,name,birth,salary,department\n', '3,Ivan,1960,130000,8']
+        json_data = """[
+    {
+        "id": "3",
+        "name": "Ivan",
+        "birth": "1960",
+        "salary": "130000",
+        "department": "8"
+    }
+]"""
+        self.assertEqual(c1.convert_csv_to_json(data), '[{"name": "3", "id": "Ivan", "birth": "1960", "salary": "130000", "department": "8"}]')
+        self.assertEqual(c2.convert_csv_to_json(data), json_data)
+
+    def test_convert_csv_to_json_with_skips(self) -> None:
+        c1 = CSVConverter()
+        c2 = CSVConverter2()
+        data = ['id,name,birth,salary,department\n', '3,Ivan,,130000,8']
+        json_data = """[
+    {
+        "id": "3",
+        "name": "Ivan",
+        "birth": "",
+        "salary": "130000",
+        "department": "8"
+    }
+]"""
+        self.assertEqual(c1.convert_csv_to_json(data), '[{"name": "3", "id": "Ivan", "birth": "", "salary": "130000", "department": "8"}]')
+        self.assertEqual(c2.convert_csv_to_json(data), json_data)
+
+if __name__ == "__main__":
+    unittest.main()
